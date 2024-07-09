02a8c93cca49abc5da24d7474857c4da1613cd58
huyluong26 <160562478+huyluong26@users.noreply.github.com>
2024-07-09 20:23:31 +0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+  $("#a1").mouseenter(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/358x/media/catalog/product/i/p/iphone15-pro-max-titan-trang.jpg');
+  });
+
+  $("#a1").mouseleave(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/i/p/iphone-15-pro-max_3.png' );
+  });
+});
+
+$(document).ready(function(){
+  $("#a2").mouseenter(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-s24-ultra_13_.png');
+  });
+
+  $("#a2").mouseleave(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-s24-ultra_12_.png' );
+  });
+
+});
+$(document).ready(function(){
+  $("#a3").mouseenter(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/d/i/dien-thoai-oppo-reno-11-f-1.png');
+  });
+
+  $("#a3").mouseleave(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/insecure/rs:fill:0:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/d/i/dien-thoai-oppo-reno-11-f-2.png' );
+  });
+});
+
+$(document).ready(function(){
+  $("#a4").mouseenter(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/x/i/xiaomi-redmi-note-13_7_.png');
+  });
+
+  $("#a4").mouseleave(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/x/i/xiaomi-redmi-note-13_11_.png' );
+  });
+});
+
+$(document).ready(function(){
+  $("#a5").mouseenter(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/s/a/samsung-galaxy-z-flip5-kem-5_1.jpg');
+  });
+
+  $("#a5").mouseleave(function(){
+      $(this).find('img').attr('src', 'https://cdn2.cellphones.com.vn/x/media/catalog/product/g/a/galaxy-z-flip-xanh-2.jpg' );
+  });
+});
+
